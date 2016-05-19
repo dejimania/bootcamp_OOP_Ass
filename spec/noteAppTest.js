08114f99fa6ce46c5547935c543580c12e08cc97
dejimania <kamilbalogun1@gmail.com>
2016-05-19 13:39:44 +0100

    delete noteApp.js

--- spec/noteAppTest.js
+++ spec/noteAppTest.js
@@ -1,4 +1,6 @@
-//some test cases for NotesApplication.js
+/*
+ Test case for the noteApplication class
+ */
  'use strict';
  
 var noteApp;
@@ -21,9 +23,9 @@ describe("NotesApplication", function(){
 	
 });
 
-// test for create method 
+// test cases for create method 
 
-describe("NotesApplication create function", function(){
+describe("NotesApplication create method", function(){
 
 	beforeEach(function() {
 		 noteApp = new NotesApplication('Kamil');
@@ -45,27 +47,37 @@ describe("NotesApplication create function", function(){
 
 });
 
+// some test cases for delete method
 
-// test for delete method 
 
-describe("NotesApplication create function", function(){
+describe("NotesApplication delete method", function(){
 
 	beforeEach(function() {
 		 noteApp = new NotesApplication('Kamil');
 		 noteApp.delete('note id');
 	});
 
-	it('First note successfully created as first index', function(){
-		expect(noteApp.[0]).toEqual('First Note');
+	it('does not allows empty note_id', function(){
+		expect(noteApp.delete('undefined')).toEqual('Please provide a valid Note ID');
 	});
 
-	it('does not allows empty note to be created', function(){
-		expect(noteApp.delete('')).toEqual('note content can not be empty!');
-	});
+});
 
-	it('assign last item in the note array to note content', function(){
-		noteApp.create('Second Note');
-		expect(noteApp.note[noteApp.note.length - 1]).toEqual('Second Note');
+// test cases for edit method 
+
+describe("NotesApplication edit method", function(){
+
+	beforeEach(function() {
+		 noteApp = new NotesApplication('Kamil');
+		 noteApp.edit('note id');
+	});
+	
+	it('does not allows note_id to be empty', function(){
+		expect(noteApp.edit('Please provide a valid Note ID')).toEqual('Invalid Note ID');
+	});
+	
+	it('Edit content cannot be empty', function(){
+		expect(noteApp.edit('')).toEqual('Please add some notes!');
 	});
 
 });
